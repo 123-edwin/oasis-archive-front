c49c6f9ebc1e82cb1ea2e2eca1fd6b07708a4f25
Isai-bot777 <erios8@ucol.mx>
2026-04-14 22:26:24 -0600

    deploy

--- src/api/oasisApi.js
+++ src/api/oasisApi.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+
 const oasisApi = axios.create({
-  baseURL: 'http://localhost:3000/api'
+  baseURL: apiUrl
 });
 
 // Interceptor para añadir el token automáticamente si existe
